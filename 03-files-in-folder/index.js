cce7216b6e256e63f257f173f4b5d4caa59ff03d
Altynay <altynaygoha2007@mail.ru>
2022-11-04 22:48:49 +0600

    fix: change tab spaces

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,19 +4,19 @@ const path = require("path");
 const secretFolder = path.join(__dirname, "secret-folder");
 
 fs.readdir(secretFolder, {
-    withFileTypes: true
+  withFileTypes: true
 }, (err, files) => {
-    err ? console.log(err) : null;
-    files.forEach((file) => {
-        if (file.isFile()) {
-            const fileName = file.name;
-            const filePath = path.join(secretFolder, fileName);
-            fs.stat(filePath, (err, stats) => {
-                const index = path.resolve(fileName);
-                const obj = path.parse(index);
-                err ? console.log(err) : null;
-                console.log(`${obj.name} - ${obj.ext.slice(1)} - ${(stats.size / 1024).toFixed(3)} kb`);
-            });
-        };
-    });
+  err ? console.log(err) : null;
+  files.forEach((file) => {
+    if (file.isFile()) {
+      const fileName = file.name;
+      const filePath = path.join(secretFolder, fileName);
+      fs.stat(filePath, (err, stats) => {
+        const index = path.resolve(fileName);
+        const obj = path.parse(index);
+        err ? console.log(err) : null;
+        console.log(`${obj.name} - ${obj.ext.slice(1)} - ${(stats.size / 1024).toFixed(3)} kb`);
+      });
+    };
+  });
 });
