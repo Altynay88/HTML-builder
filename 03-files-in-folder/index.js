4f8bef9392692ad276b6f1a2949b89eb5c07b06f
Altynay <altynaygoha2007@mail.ru>
2022-11-04 21:05:50 +0600

    feat: add a solution to 03-files-in-folder task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const path = require("path");
+
+const secretFolder = path.join(__dirname, "secret-folder");
+
+fs.readdir(secretFolder, {
+    withFileTypes: true
+}, (err, files) => {
+    err ? console.log(err) : null;
+    files.forEach((file) => {
+        if (file.isFile()) {
+            const fileName = file.name;
+            const filePath = path.join(secretFolder, fileName);
+            fs.stat(filePath, (err, stats) => {
+                const index = path.resolve(fileName);
+                const obj = path.parse(index);
+                err ? console.log(err) : null;
+                console.log(`${obj.name} - ${obj.ext.slice(1)} - ${(stats.size / 1024).toFixed(3)} kb`);
+            });
+        };
+    });
+});
